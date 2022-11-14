b85b82332574258149426f51b1695b971c919680
kwhn9486@naver.com <kwhn9486@naver.com>
2022-11-14 11:58:15 +0900

    Login & Register ViewController

--- ChatApp/ChatApp/Controllers/ConversationsViewController.swift
+++ ChatApp/ChatApp/Controllers/ConversationsViewController.swift
@@ -12,8 +12,24 @@ class ConversationsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let isLoggedIn = UserDefaults.standard.bool(forKey: "logged_in")
+        
+        if !isLoggedIn {
+            let vc = LoginViewController()
+            let nav = UINavigationController(rootViewController: vc)
+            nav.modalPresentationStyle = .fullScreen
+            
+            present(nav, animated: true)
+            //animated -> modal 창이 나타날 때 애니메이션 효과를 줄지 말지 여부 표시
+        }
+    }
+    
 
 }
 
